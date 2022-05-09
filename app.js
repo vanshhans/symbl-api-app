5097621907230d404943c71f1bb41b293cef07ab
vansh hans <vanshhans9@gmail.com>
2022-05-09 21:16:25 +0530

    basic file upload to express

--- app.js
+++ app.js
@@ -1,11 +1,28 @@
 const express=require("express");
-const app=express();
+const multer = require("multer");
+const upload =multer({storage});
 
+const app=express();
+const storage = multer.diskStorage({
+    filename: function (req, file, cb) {
+      console.log('filename')
+      cb(null, file.originalname)
+    },
+    destination: function (req, file, cb) {
+      console.log('storage')
+      cb(null, './uploads')
+    },
+  })
 
 app.get("/",(req,res)=>{
     res.send("home page");
 })
 
+app.post("/api.symbl.ai/v1/process/audio",upload.single('file'),(req,res)=>{
+    console.log(req.body);
+    console.log(req.file);
+    res.send("successfully uploaded the file");
+});
 
 
 app.listen(3000,()=>{
