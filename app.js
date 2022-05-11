0f220bdaea9290d265f67941d06ec8779ab03dd0
vansh hans <vanshhans9@gmail.com>
2022-05-12 01:19:26 +0530

    almost done

--- app.js
+++ app.js
@@ -3,28 +3,33 @@ const multer = require("multer");
 const path = require("path");
 const request = require("request");
 const fs = require("fs");
-// const tokenGenerator=require("./auth_token");
+const {
+  response
+} = require("express");
 
 const app = express();
 const storage = multer.diskStorage({
   filename: function (req, file, cb) {
-    console.log("filename");
+    // console.log("filename");
     cb(null, file.originalname);
   },
 });
 
-const upload = multer({ storage });
+const upload = multer({
+  storage
+});
+const authToken =
+  "eyJhbGciOiJSUzI1NiIsInR5cCI6IkpXVCIsImtpZCI6IlFVUTRNemhDUVVWQk1rTkJNemszUTBNMlFVVTRRekkyUmpWQ056VTJRelUxUTBVeE5EZzFNUSJ9.eyJodHRwczovL3BsYXRmb3JtLnN5bWJsLmFpL3VzZXJJZCI6IjYzNjMyMDUxODUwNDQ0ODAiLCJpc3MiOiJodHRwczovL2RpcmVjdC1wbGF0Zm9ybS5hdXRoMC5jb20vIiwic3ViIjoiR3ZZMzlpNDNZcHpORzVTVTF1a1JoUDdJcU54Q1lVWGZAY2xpZW50cyIsImF1ZCI6Imh0dHBzOi8vcGxhdGZvcm0ucmFtbWVyLmFpIiwiaWF0IjoxNjUyMjc4MTI4LCJleHAiOjE2NTIzNjQ1MjgsImF6cCI6Ikd2WTM5aTQzWXB6Tkc1U1UxdWtSaFA3SXFOeENZVVhmIiwiZ3R5IjoiY2xpZW50LWNyZWRlbnRpYWxzIn0.z41bLxgcWxbK0hD-XbvCCrUo4AFA6Cnff5LRSF7X-1iJs9MgXtCY4JJ6f1nVXqop2F4RfpgOGFEAzdcBf0HwAeI3g3dDh-hUoaBHAQHN85fvur6QQkkPJqMobshdF9VPS7bNJpOTjwRwXNNfZ7G4S5sXFmhJAdLy82SMKtAKoMChKOuniUsd0xN7mLixPt5hs62Pj_puzEZrkNzXWLhSyr7N2zBpysfRhlaqjN-jxM877tgxo4mCeA6TxwDQqfegA3Cl9QXp9QWdhEm-mw3g-kMbHiQKF-XrT7X5IV0_uEOjLFZ9oX6hZ60KdrCbSKobl7SYjPepRR8lo70IgL2D0A";
 
-app.use(express.static(path.join(__dirname, "public")));
+app.set("view engine", "ejs");
+
+app.use(express.static(path.join(__dirname)));
 
 app.get("/", (req, res) => {
   res.render("./index.html");
 });
 
 app.post("/audio", upload.single("file"), (req, res) => {
-  const authToken =
-    "eyJhbGciOiJSUzI1NiIsInR5cCI6IkpXVCIsImtpZCI6IlFVUTRNemhDUVVWQk1rTkJNemszUTBNMlFVVTRRekkyUmpWQ056VTJRelUxUTBVeE5EZzFNUSJ9.eyJodHRwczovL3BsYXRmb3JtLnN5bWJsLmFpL3VzZXJJZCI6IjYzNjMyMDUxODUwNDQ0ODAiLCJpc3MiOiJodHRwczovL2RpcmVjdC1wbGF0Zm9ybS5hdXRoMC5jb20vIiwic3ViIjoiR3ZZMzlpNDNZcHpORzVTVTF1a1JoUDdJcU54Q1lVWGZAY2xpZW50cyIsImF1ZCI6Imh0dHBzOi8vcGxhdGZvcm0ucmFtbWVyLmFpIiwiaWF0IjoxNjUyMTkyMzU3LCJleHAiOjE2NTIyNzg3NTcsImF6cCI6Ikd2WTM5aTQzWXB6Tkc1U1UxdWtSaFA3SXFOeENZVVhmIiwiZ3R5IjoiY2xpZW50LWNyZWRlbnRpYWxzIn0.sw4hRSiobDvJg6yukdbJ67a7s3ZNwnJ58aAPjpcsx01LeY7ujR3yE9oYj8ZtPMa1jZ-IoWH0kdF877G4g_2icdHNv79uP6ZTvs0Hp9ImMMNwT1dn1NtH6QTZPJHbxamzXzeEzKu6MHXYsd_bt8LqNC0e0sAAHlcSIjvQ5jCtQempbZXEtuQIFzmHGWpjIZlydZnDnqz6xV02JYDlU8bKNk4B86-lE0NKM1DusS257NNg02nJhxueSSbQsG_8tKySnBgUkY9LLmBwIYNS4_cxC5YIbQjJ9LNWhvouR-U6KeoAsD3WG8vRsI3MNkuO1EnW0BdauDnBaRxRw1CA-bXOXQ";
-  //const webhookUrl = WEBHOOK_URL;
   const audioFileStream = fs.createReadStream(`${req.file.filename}`);
 
   const audioOption = {
@@ -43,11 +48,80 @@ app.post("/audio", upload.single("file"), (req, res) => {
       }
       console.log("Status code: ", statusCode);
       console.log("Body", response.body);
+      global.conversationId = JSON.stringify(response.body["conversationId"]);
+      // console.log(JSON.stringify(response.body.conversationId));
+      // console.log(global.conversationId);
+      res.render("home.ejs", {
+        conversationId: response.body["conversationId"]
+      });
     })
   );
-
-  res.send("successfully uploaded the file");
 });
+
+app.get("/get-Speech-To-Text/:conversationId", (req, res) => {
+  const id = req.params;
+  request.get({
+    url: `https://api.symbl.ai/v1/conversations/${id["conversationId"]}/messages`,
+    headers: {
+      'Authorization': `Bearer ${authToken}`
+    },
+    json: true
+  }, (err, response, body) => {
+    console.log(body);
+  });
+})
+
+app.get("/get-Speech-To-Text/:conversationId", (req, res) => {
+  const id = req.params;
+  request.get({
+    url: `https://api.symbl.ai/v1/conversations/${id["conversationId"]}/action-items`,
+    headers: {
+      'Authorization': `Bearer ${authToken}`
+    },
+    json: true
+  }, (err, response, body) => {
+    console.log(body);
+  });
+})
+
+app.get("/get-Speech-To-Text/:conversationId", (req, res) => {
+  const id = req.params;
+  request.get({
+    url: `https://api.symbl.ai/v1/conversations/${id["conversationId"]}/follow-ups`,
+    headers: {
+      'Authorization': `Bearer ${authToken}`
+    },
+    json: true
+  }, (err, response, body) => {
+    console.log(body);
+  });
+})
+
+app.get("/get-Speech-To-Text/:conversationId", (req, res) => {
+  const id = req.params;
+  request.get({
+    url: `https://api.symbl.ai/v1/conversations/${id["conversationId"]}/topics`,
+    headers: {
+      'Authorization': `Bearer ${authToken}`
+    },
+    json: true
+  }, (err, response, body) => {
+    console.log(body);
+  });
+})
+
+app.get("/get-Speech-To-Text/:conversationId", (req, res) => {
+  const id = req.params;
+  request.get({
+    url: `https://api.symbl.ai/v1/conversations/${id["conversationId"]}/questions`,
+    headers: {
+      'Authorization': `Bearer ${authToken}`
+    },
+    json: true
+  }, (err, response, body) => {
+    console.log(body);
+  });
+})
 
 app.listen(3000, () => {
   console.log("Listening on port 3000");
